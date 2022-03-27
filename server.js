06aaa4b602ab51b73f1d6872d207c53fb075ee04
agmorrow <morrow.austin5@gmail.com>
2022-03-27 17:41:35 -0600

    added userRoutes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
-const USer = require('./models/User');
+const routes = require('./routes');
 const sequelize = require('./config');
 
 const hbs = exphbs.create({});
@@ -16,6 +16,7 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
