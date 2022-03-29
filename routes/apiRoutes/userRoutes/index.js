a7fa350be8a5a46f8d69fa6f13459221b8c21c7c
agmorrow <morrow.austin5@gmail.com>
2022-03-29 01:09:27 -0600

    final push

--- routes/apiRoutes/userRoutes/index.js
+++ routes/apiRoutes/userRoutes/index.js
@@ -1,20 +1,25 @@
+
 const router = require('express').Router();
 const {
-  createUser,
-  getUserById,
-  login,
-  signupHandler,
-  logout,
+	createUser,
+	getUserById,
+	login,
+	signupHandler,
+	logout,
 } = require('../../../controllers/userController');
-
+// /api/users
 router.route('/')
-  .post(createUser);
+	.post(createUser);
 
-  router.post('/signup', signupHandler);
-  router.post('/login', login);
-  router.post('/logout', logout);
+router.post('/signup', signupHandler);
+router.post('/login', login);
+router.post('/logout', logout);
 
-  router.route('/:userId')
-  .get(getUserById);
-
+router.route('/:userId')
+	.get(getUserById);
+// /api/users/:userId
+// router.route('/:userId')
+// 	.delete(deleteUserById)
+// 	.get(getUserById)
+// 	.put(updateUserById)
 module.exports = router;
